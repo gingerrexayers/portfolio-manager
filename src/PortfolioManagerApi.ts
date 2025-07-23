031c1b0f2b3af9916b7ab4668011b083d63ac765
Ginger Ayers <ginger.rex.ayers@gmail.com>
2025-07-23 00:56:44 -0700

    refactor: enhance xmlParserOptions to ensure consistent array handling for various jpath values

--- src/PortfolioManagerApi.ts
+++ src/PortfolioManagerApi.ts
@@ -97,22 +97,20 @@ export class PortfolioManagerApi {
   xmlParserOptions: Partial<X2jOptions> = {
     ignoreAttributes: false,
     isArray: (name, jpath, isLeafNode, isAttribute): boolean => {
-      // ensure response.links.link is always an array even when there
-      // is only one link which results in  object by default
-      // console.log(jpath);
       return (
         jpath === "response.links.link" ||
         jpath === "propertyMetrics.metric" ||
         jpath === "meterData.links.link" ||
         jpath ===
-          "meterPropertyAssociationList.waterMeterAssociation.meters.meterId" ||
+        "meterPropertyAssociationList.waterMeterAssociation.meters.meterId" ||
         jpath ===
-          "meterPropertyAssociationList.energyMeterAssociation.meters.meterId" ||
+        "meterPropertyAssociationList.energyMeterAssociation.meters.meterId" ||
         jpath ===
-          "meterPropertyAssociationList.wasteMeterAssociation.meters.meterId" ||
+        "meterPropertyAssociationList.wasteMeterAssociation.meters.meterId" ||
         jpath === "meterData.meterDelivery" ||
         jpath === "meterData.meterConsumption" ||
         jpath === "additionalIdentifiers.additionalIdentifier" ||
+        jpath === "pendingList.links.link" ||
         jpath === "pendingList.property" ||
         jpath === "pendingList.account" ||
         jpath === "pendingList.meter"
